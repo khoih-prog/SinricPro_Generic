1b110bcbe21639a1f70f5228ad526a3aa12c28fe
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-05-24 02:20:15 -0400

    v2.4.0
    ### Releases v2.4.0
    
    1. Add support to :
    
      - ***Arduino SAMD21 (ZERO, MKR, NANO_33_IOT, M0, M0 Pro, AdaFruit CIRCUITPLAYGROUND_EXPRESS, etc.)***.
      - ***Adafruit SAM21 (Itsy-Bitsy M0, Metro M0, Feather M0, Gemma M0, etc.)***.
      - ***Adafruit SAM51 (Itsy-Bitsy M4, Metro M4, Grand Central M4, Feather M4 Express, etc.)***.
      - ***AdaFruit Feather nRF52832, nRF52840 Express, BlueFruit Sense, Itsy-Bitsy nRF52840 Express, Metro nRF52840 Express, NINA_B30_ublox, etc.***.
      - ***Teensy 4.1, 4.0, 3.6, 3.5, 3.2/3.1, 3.0.***
      - ***STM32 (Nucleo-144, Nucleo-64, Nucleo-32, Discovery, STM32F1, STM32F3, STM32F4, STM32H7, STM32L0, etc.)***.
      - ***SAM DUE***
      
    2. Bump up to sync with v2.4.0 of original SinricPro library

---
+++ examples/ESP/Switch/MultiSwitch_intermediate/MultiSwitch_intermediate.ino
@@ -0,0 +1,95 @@
+/*
+ * Simple example for how to use multiple SinricPro Switch devices:
+ * - setup 4 switch devices using an array
+ * - handle request using single callback
+ * 
+ * If you encounter any issues:
+ * - check the readme.md at https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md
+ * - ensure all dependent libraries are installed
+ *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#arduinoide
+ *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#dependencies
+ * - open serial monitor and check whats happening
+ * - check full user documentation at https://sinricpro.github.io/esp8266-esp32-sdk
+ * - visit https://github.com/sinricpro/esp8266-esp32-sdk/issues and check for existing issues or open a new one
+ */
+
+// Uncomment the following line to enable serial debug output
+//#define ENABLE_DEBUG
+
+#ifdef ENABLE_DEBUG
+       #define DEBUG_ESP_PORT Serial
+       #define NODEBUG_WEBSOCKETS
+       #define NDEBUG
+#endif 
+
+#include <Arduino.h>
+#ifdef ESP8266 
+       #include <ESP8266WiFi.h>
+#endif 
+#ifdef ESP32   
+       #include <WiFi.h>
+#endif
+
+#include "SinricPro.h"
+#include "SinricProSwitch.h"
+
+#define WIFI_SSID         "YOUR-WIFI-SSID"    
+#define WIFI_PASS         "YOUR-WIFI-PASSWORD"
+#define APP_KEY           "YOUR-APP-KEY"      // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
+#define APP_SECRET        "YOUR-APP-SECRET"   // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
+#define BAUD_RATE         9600                // Change baudrate to your need
+
+#define DEVICES           4                   // define how much devices are in SWITCH_IDs array
+String SWITCH_IDs[DEVICES] = {                // define deviceIds in an array
+  "YOUR_DEVICE_ID_1",
+  "YOUR_DEVICE_ID_2",
+  "YOUR_DEVICE_ID_3",
+  "YOUR_DEVICE_ID_4"
+};
+
+bool onPowerState(const String &deviceId, bool &state) {
+  for (int i=0; i < DEVICES; i++) {     // go through the devices
+    if (deviceId == SWITCH_IDs[i]) {    // if deviceId matches
+      Serial.printf("Device number %i turned %s\r\n", i, state?"on":"off");   // print power state for device
+    }
+  }
+  return true; // request handled properly
+}
+
+// setup function for WiFi connection
+void setupWiFi() {
+  Serial.printf("\r\n[Wifi]: Connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf(".");
+    delay(250);
+  }
+
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
+}
+
+// setup function for SinricPro
+void setupSinricPro() {
+  // add devices to SinricPro and set callback function
+  for (int i = 0; i < DEVICES; i++) {
+    SinricProSwitch& mySwitch = SinricPro[SWITCH_IDs[i]];
+    mySwitch.onPowerState(onPowerState);
+  }
+
+  // setup SinricPro
+  SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); }); 
+  SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
+  SinricPro.begin(APP_KEY, APP_SECRET);
+}
+
+// main setup function
+void setup() {
+  Serial.begin(BAUD_RATE); Serial.printf("\r\n\r\n");
+  setupWiFi();
+  setupSinricPro();
+}
+
+void loop() {
+  SinricPro.handle();
+}
