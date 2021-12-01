be62d561609724ef0e6db214916dba19f553cb5e
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2021-12-01 16:23:16 -0500

    v2.8.4 to auto-detect ESP32 core version
    ## Version 2.8.4
    
    1. Auto detect ESP32 core version.
    2. Fix bug in examples for WT32_ETH01
    3. Update `Packages' Patches`

--- examples/WT32_ETH01/Light/Light/Light.ino
+++ examples/WT32_ETH01/Light/Light/Light.ino
@@ -32,6 +32,8 @@
   #define NDEBUG
 #endif
 
+#define _SRP_LOGLEVEL_       4
+
 #define WEBSOCKET_SSL     true
 
 #include <WebServer_WT32_ETH01.h>
@@ -171,6 +173,10 @@ bool onDecreaseColorTemperature(const String &deviceId, int &colorTemperature)
 void setupETH() 
 {
   Serial.print("\n[ETH]: Connecting");
+  
+  // To be called before ETH.begin()
+  WT32_ETH01_onEvent();
+
   //bool begin(uint8_t phy_addr=ETH_PHY_ADDR, int power=ETH_PHY_POWER, int mdc=ETH_PHY_MDC, int mdio=ETH_PHY_MDIO, 
   //           eth_phy_type_t type=ETH_PHY_TYPE, eth_clock_mode_t clk_mode=ETH_CLK_MODE);
   //ETH.begin(ETH_PHY_ADDR, ETH_PHY_POWER, ETH_PHY_MDC, ETH_PHY_MDIO, ETH_PHY_TYPE, ETH_CLK_MODE);
@@ -179,8 +185,6 @@ void setupETH()
   // Static IP, leave without this line to get IP via DHCP
   //bool config(IPAddress local_ip, IPAddress gateway, IPAddress subnet, IPAddress dns1 = 0, IPAddress dns2 = 0);
   ETH.config(myIP, myGW, mySN, myDNS);
-
-  WT32_ETH01_onEvent();
 
   WT32_ETH01_waitForConnect();
   
--- examples/WT32_ETH01/Thermostat/Thermostat.ino
+++ examples/WT32_ETH01/Thermostat/Thermostat.ino
@@ -94,6 +94,10 @@ bool onThermostatMode(const String &deviceId, String &mode)
 void setupETH() 
 {
   Serial.print("[ETH]: Connecting");
+  
+  // To be called before ETH.begin()
+  WT32_ETH01_onEvent();
+
   //bool begin(uint8_t phy_addr=ETH_PHY_ADDR, int power=ETH_PHY_POWER, int mdc=ETH_PHY_MDC, int mdio=ETH_PHY_MDIO, 
   //           eth_phy_type_t type=ETH_PHY_TYPE, eth_clock_mode_t clk_mode=ETH_CLK_MODE);
   //ETH.begin(ETH_PHY_ADDR, ETH_PHY_POWER, ETH_PHY_MDC, ETH_PHY_MDIO, ETH_PHY_TYPE, ETH_CLK_MODE);
@@ -102,8 +106,6 @@ void setupETH()
   // Static IP, leave without this line to get IP via DHCP
   //bool config(IPAddress local_ip, IPAddress gateway, IPAddress subnet, IPAddress dns1 = 0, IPAddress dns2 = 0);
   ETH.config(myIP, myGW, mySN, myDNS);
-
-  WT32_ETH01_onEvent();
 
   WT32_ETH01_waitForConnect();
   
